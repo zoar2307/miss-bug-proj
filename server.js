5527ae75be2966302bb4b7ee1cc8617ca35286bd
zoar2307 <zoar2307@gmail.com>
2024-10-21 14:43:04 +0300

    Add cookies

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { loggerService } from './services/logger.service.js'
 import { bugService } from './services/bug.service.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 const port = 3030
 
 app.use(express.static('public'))
+app.use(cookieParser())
 
 app.get('/api/bug', (req, res) => {
     bugService.query()
@@ -24,6 +26,16 @@ app.get('/api/bug/save', (req, res) => {
 app.get('/api/bug/:bugId', (req, res) => {
     const { bugId } = req.params
     bugService.getById(bugId)
+        .then(bug => {
+            let visitedBugs = req.cookies.visitedBugs || []
+            if (visitedBugs.length === 3) return res.status(401).send('Wait for a bit')
+            if (!visitedBugs.some(visitedBugId => visitedBugId === bugId)) {
+                visitedBugs.push(bugId)
+                res.cookie('visitedBugs', visitedBugs, { maxAge: 1000 * 7 })
+            }
+            return bug
+
+        })
         .then(bug => res.send(bug))
         .catch(err => loggerService.error('Cant get bug' + err))
 })

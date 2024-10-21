b7b48939d0d5971142128af9b597c486179a197f
zoar2307 <zoar2307@gmail.com>
2024-10-21 13:28:27 +0300

    Add CRUDL

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import { loggerService } from './services/logger.service.js'
+import { bugService } from './services/bug.service.js'
+
+const app = express()
+const port = 3030
+
+app.get('/api/bug', (req, res) => {
+    bugService.query()
+        .then(bugs => res.send(bugs))
+        .catch(err => loggerService.error('Cant load bugs' + err))
+})
+
+app.get('/api/bug/save', (req, res) => {
+    const { _id, title, description, severity } = req.query
+    const bugToSave = { _id, title, description, severity: +severity }
+    bugService.save(bugToSave)
+        .then(bug => res.send(bug))
+        .catch(err => loggerService.error('Cant save bug' + err))
+})
+
+app.get('/api/bug/:bugId', (req, res) => {
+    const { bugId } = req.params
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => loggerService.error('Cant get bug' + err))
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const { bugId } = req.params
+    bugService.remove(bugId)
+        .then(() => res.send(`Bug ${bugId} removed`))
+        .catch(err => loggerService.error('Cant remove bug' + err))
+})
+
+app.listen(port, () =>
+    loggerService.info(`Server is ready on http://127.0.0.1:${port}/`))
---
+++ services/bug.service.js
@@ -0,0 +1,56 @@
+import { utilService } from "./util.service.js"
+import fs from 'fs'
+
+
+const bugs = utilService.readJsonFile('./data/bugs.json')
+
+export const bugService = {
+    query,
+    getById,
+    remove,
+    save
+}
+
+function query() {
+    return Promise.resolve(bugs)
+}
+
+function getById(bugId) {
+    const selectBug = bugs.find(bug => bug._id === bugId)
+    if (!selectBug) return Promise.reject(`Cant find ${bugId}`)
+    return Promise.resolve(selectBug)
+}
+
+function remove(bugId) {
+    const selectBugIdx = bugs.findIndex(bug => bug._id === bugId)
+    if (selectBugIdx < 0) return Promise.reject(`Cant find ${bugId}`)
+    bugs.splice(selectBugIdx, 1)
+    return _saveBugsToFile()
+}
+
+function save(bugToSave) {
+    if (bugToSave._id) {
+        const bugIdx = bugs.findIndex(bug => bug._id === bugToSave._id)
+        bugs[bugIdx] = bugToSave
+    } else {
+        bugToSave._id = utilService.makeId()
+        bugToSave.createdAt = Date.now()
+        bugs.unshift(bugToSave)
+    }
+
+    return _saveBugsToFile()
+        .then(() => bugToSave)
+}
+
+
+function _saveBugsToFile() {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(bugs, null, 4)
+        fs.writeFile('data/bugs.json', data, (err) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
